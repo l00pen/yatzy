6e4cb0f6f77937ae010aabc3a152d33051648f65
l00pen <lovisa.skeppe@gmail.com>
2019-07-09 19:37:44 +0200

    add linting

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
-import BrickBraker from 'Components/BrickBraker';
+import React from 'react';
 import Dashboard from 'Components/Dashboard';
 import { connect } from 'react-redux';
-import { startGame, stopGame } from '../../actions/gameActions';
+
 import Yatzy from 'Components/Yatzy';
 
 import './styles.css';
-
-function newGameInstance(id, user) {
-  return new BrickBraker(id, user);
-}
 
 const App = ({ user }) => {
   return (
@@ -20,7 +15,7 @@ const App = ({ user }) => {
       <Yatzy />
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   return {

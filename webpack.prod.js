eb2065ce90c4d52dfa2f0ba1ea1edd4eaa0a92a2
Lovisa <lovisa.skeppe@gmail.com>
2024-09-18 10:52:02 +0200

    add cache invalidation for new builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
+  output: {
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    minimize: true,
+  },
   plugins: [],
 });

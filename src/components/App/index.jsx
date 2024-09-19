66dd09f23fdc9416510db091b2b7519039b6a1cf
Lovisa <lovisa.skeppe@gmail.com>
2024-09-19 14:15:50 +0200

    add a gradient background

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,57 +1,46 @@
+
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
 import React from "react";
 import { connect } from "react-redux";
-
 import Yatzy from "Components/Yatzy";
-
 import CssBaseline from '@mui/material/CssBaseline';
 import { Container, useMediaQuery, useTheme } from '@mui/material';
+import { css, keyframes } from "@emotion/react";
 
+
+const gradientBackgroundEffect = keyframes`
+   0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+`;
 
 const App = ({ }) => {
+  const animatedBackground = css`
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 600% 600%;
+    animation: ${gradientBackgroundEffect} 10s ease infinite;
+    height: 100%;
+    overflow: auto;
+    padding-bottom: 50px;
+  `;
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <>
       <CssBaseline />
-        <div style={{
-          backgroundColor: 'black',
-          position: 'relative',
-          height: '100vh',
-          overflow: 'hidden',
-        }}>
-      <div style={{
-        width: '100%',
-        height: '100%',
-        borderRadius: '50%',
-        filter: 'blur(25px)',
-        position: 'absolute',
-        bottom: '-70%',  
-        background: 'radial-gradient(circle, #1e237f, transparent)',
-        zIndex: 0,
-      }}/>
-      <div style={{
-        width: '100%',
-        height: '50%',
-        borderRadius: '50%',
-        filter: 'blur(25px)',
-        position: 'absolute',
-        left: '-40%',  
-        background: 'radial-gradient(circle at center, blue, transparent 40%)',
-        zIndex: 0,
-      }}/>
-      <div style={{
-        position: 'relative',  
-        zIndex: 1,
-        overflow: 'auto', 
-        height: '100vh',
-        paddingBottom: '100px',   
-      }}>
-      <Container maxWidth={isMobile ? 'lg'  : 'sm'} >
-        <Yatzy />
-      </Container>
-        </div>
-        </div>
+      <div css={animatedBackground} >
+        <Container maxWidth={isMobile ? 'lg'  : 'sm'} >
+          <Yatzy />
+        </Container>
+      </div>
     </>
   );
 };
